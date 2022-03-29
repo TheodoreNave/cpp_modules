58ed63f2f3e0926e421b3249d4c0ad7f20d8d104
Theodore NAVE <tnave@e1r12p3.42.fr>
2022-03-29 16:33:02 +0200

    added exercise 2

---
+++ module05/ex03/Bureaucrat.hpp
@@ -0,0 +1,53 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <string>
+#include <iostream>
+#include <stdexcept>
+#include "AForm.hpp"
+
+class AForm;
+
+class Bureaucrat {
+
+	private:
+
+		std::string _name;
+		int 		_grade;
+
+	class GradeTooHighException  : public std::exception {
+
+		public:
+
+			virtual const char* what() const throw();
+	};
+
+	class GradeTooLowException  : public std::exception {
+
+		public:
+
+			virtual const char* what() const throw();
+	};
+
+	public:
+
+		Bureaucrat( void );
+		Bureaucrat( std::string name, int grade );
+		Bureaucrat( Bureaucrat const &src );
+		~Bureaucrat( void );
+
+		Bureaucrat &operator=(Bureaucrat const &rhs);
+
+		std::string getName( void ) const ;
+		int getGrade( void ) const ;
+		void signForm(AForm &form);
+		void increm( void );
+		void decrem( void );
+		void executeForm( AForm const & form );
+
+
+};
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &rhs);
+
+#endif
